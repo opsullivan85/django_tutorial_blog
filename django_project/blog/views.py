8662a75befaedc47297003e6ae650a8ab68822a4
Owen Sullivan <opsullivan85@gmail.com>
2022-02-15 13:49:12 -0500

    Added base.html for template inheritance

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,16 +1,33 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+posts = [
+    {
+        'author': 'opsullivan85',
+        'title': 'Blog Post 1',
+        'content': 'Hello World!',
+        'date_posted': '2022/02/15'
+    },
+    {
+        'author': 'Anonomous',
+        'title': 'Blog Post 2',
+        'content': 'Hi Earth!',
+        'date_posted': '2022/02/16'
+    }
+]
 
 def home(request):
     '''
     home page
     '''
-    return render(request, 'blog/home.html', {})
+    context = {
+        'posts': posts,
+    }
+    return render(request, 'blog/home.html', context)
 
 
 def about(request):
     '''
     about page
     '''
-    return render(request, 'blog/home.html', {})
+    return render(request, 'blog/about.html', {'title': 'about'})
